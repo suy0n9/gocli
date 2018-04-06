f7b69bfc4c975add137de2bd237f1dade4f853e1
suy0n9 <suy01v9@gmail.com>
2018-04-06 22:06:39 +0900

    first commit

---
+++ cmd/head/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func run() error {
+	var (
+		number  int
+		verbose bool
+	)
+	flag.IntVar(&number, "n", 10, "number flag")
+	flag.BoolVar(&verbose, "v", false, "print file")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 1 {
+		return fmt.Errorf("need args")
+	}
+
+	file := args[0]
+	if verbose {
+		fmt.Printf("==> %s <===\n", file)
+	}
+	fp, err := os.Open(file)
+
+	if err != nil {
+		return fmt.Errorf("not read file %s", file)
+	}
+	defer fp.Close()
+
+	i := 0
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		i++
+		if i == number {
+			break
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+
+}
